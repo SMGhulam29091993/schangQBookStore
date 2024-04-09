9d09a00a458aa32f0377d7d6436497ff3950c29c
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-04-09 22:02:26 +0530

    created the necesary pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { lazy } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const Home = lazy(()=>import("./Pages/Home"));
+const About = lazy(()=>import("./Pages/About"));
+const Search = lazy(()=>import("./Pages/Search"));
+const Books = lazy(()=>import("./Pages/Books"));
+const PageNotFound = lazy(()=>import("./Pages/PageNotFound"));
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<div>Home</div>} />
-        <Route path='/about' element={<div>About</div>} />
-        <Route path='/books/:bookID' element={<div>Book Page</div>} />
-        <Route path='/search' element={<div>Search</div>} />
-        <Route path='*' element={<div>Page Not Found</div>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/books/:bookID' element={<Books />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   )
