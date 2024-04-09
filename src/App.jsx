81323fc3f262d7c9b58bf011405733aa60aff7ce
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-04-09 21:43:31 +0530

    created a relevant routes for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Home</div>} />
+        <Route path='/about' element={<div>About</div>} />
+        <Route path='/books/:bookID' element={<div>Book Page</div>} />
+        <Route path='/search' element={<div>Search</div>} />
+        <Route path='*' element={<div>Page Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
