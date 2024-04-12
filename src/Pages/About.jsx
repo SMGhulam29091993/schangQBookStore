664a7b23fe0c3b86ab497e1fd93a0e7f58604636
S M Ghulam Ghaus Faiyaz <smghulam.ghausfaiyaz19@gmail.com>
2024-04-12 18:24:41 +0530

    updated the about page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h1 className='text-2xl m-2 text-orange-500 font-semibold text-center'>About</h1>
+      <p className='p-4'>About Our Book Explorer Web Application
+
+      Welcome to our Book Explorer Web Application! Our goal is to provide users with an immersive 
+      experience where they can explore and discover a diverse collection of books. Powered by ReactJS
+      and designed with a focus on responsiveness, our web app aims to showcase our expertise in frontend 
+      development while offering users a seamless browsing experience.</p>
+
+
+    </>
+    
   )
 }
 
